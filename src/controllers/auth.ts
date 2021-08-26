5da2aa21308786834bbb1a6736c5ca8867a38d24
sakuru9 <miokillian@gmail.com>
2021-08-27 01:11:43 +0300

    login and refresh token logic

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,29 @@
-import { NextFunction, Request, Response } from "express";
+import { CookieOptions, NextFunction, Request, Response } from "express";
 import { User } from "../database/user";
-import { BadRequest, Conflict } from "http-errors";
+import { BadRequest, Conflict, Unauthorized } from "http-errors";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+require("dotenv").config();
+
+const accessTokenCookieOpts: CookieOptions = {
+  secure: true,
+  httpOnly: true,
+  sameSite: "lax",
+  maxAge: 1 * 60 * 60 * 1000,
+};
+
+const refreshTokenCookieOpts: CookieOptions = {
+  secure: true,
+  httpOnly: true,
+  sameSite: "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+};
+
+interface Token {
+  id: number;
+  iat: number;
+  exp: number;
+}
 
 const registerUser = async (
   req: Request,
@@ -30,4 +52,65 @@ const registerUser = async (
   }
 };
 
-export { registerUser };
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { username, password } = req.body;
+    const user = (await User.query().where("username", "=", username))[0];
+    if (!user) throw new Unauthorized();
+
+    const compareCheck = await bcrypt.compare(password, user.password);
+    if (!compareCheck) throw new Unauthorized();
+
+    const accessToken = getAccessToken(user.id);
+    const refreshToken = getRefreshToken(user.id);
+
+    res.cookie("hltoken", accessToken, accessTokenCookieOpts);
+    res.cookie("hlrtoken", refreshToken, refreshTokenCookieOpts);
+
+    res.status(200).send();
+  } catch (e) {
+    return next(e);
+  }
+};
+
+const refreshTokens = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { hlrtoken } = req.cookies;
+    if (!hlrtoken) throw new Unauthorized();
+
+    const jwtVerify = jwt.verify(
+      hlrtoken,
+      process.env.REFRESH_TOKEN_SECRET as string,
+    ) as Token;
+
+    // Check if refresh token has expired
+    if (jwtVerify.exp < new Date().getTime() / 1000) {
+      throw new Unauthorized();
+    }
+
+    // Token is valid, set new cookies
+    const accessToken = getAccessToken(jwtVerify.id);
+    const refreshToken = getRefreshToken(jwtVerify.id);
+
+    res.cookie("hltoken", accessToken, accessTokenCookieOpts);
+    res.cookie("hlrtoken", refreshToken, refreshTokenCookieOpts);
+
+    res.status(200).send();
+  } catch (e) {
+    throw new Unauthorized();
+  }
+};
+
+const getRefreshToken = (userId: number) => {
+  return jwt.sign({ id: userId }, process.env.REFRESH_TOKEN_SECRET as string, {
+    expiresIn: `${7 * 24 * 3600}s`,
+  });
+};
+
+const getAccessToken = (userId: number) => {
+  return jwt.sign({ uid: userId }, process.env.ACCESS_TOKEN_SECRET as string, {
+    expiresIn: "3600s",
+  });
+};
+
+export { registerUser, login, refreshTokens };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth";
 import { initDb } from "./database";
+import { cors } from "./middlewares/cors";
+
+require("dotenv").config();
 
 const app = express();
 
+app.use(cookieParser());
+app.use(cors);
 app.use(express.json());
+
 app.use("/auth", authRouter);
 
 initDb();
